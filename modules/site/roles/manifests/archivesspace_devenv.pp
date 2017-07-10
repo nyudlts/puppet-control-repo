4aba443470263b97800a366208ea1b8615f81c15
Flannon Jackson <flannon@nyu.edu>
2017-07-10 15:04:15 -0400

    Commented out profiles::rbenv_provile

--- modules/site/roles/manifests/archivesspace_devenv.pp
+++ modules/site/roles/manifests/archivesspace_devenv.pp
@@ -3,5 +3,5 @@ class roles::archivesspace_devenv {
     include profiles::base
     #require profiles::db_mysql
     include profiles::dev_aspace_devenv
-    include profiles::rbenv_profile
+    #include profiles::rbenv_profile
 }
