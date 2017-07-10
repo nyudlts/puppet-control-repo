8d73c4260711cecc9e49fb6639db18d9161df726
Flannon Jackson <flannon@nyu.edu>
2017-07-09 23:29:48 -0400

    Install golang to /opt /go from the go rpm

--- modules/local/golang/manifests/env.pp
+++ modules/local/golang/manifests/env.pp
@@ -9,20 +9,32 @@ define golang::env {
 
   file_line { "${userhome} goroot":
     path => "${userhome}/.bashrc",
-    line => 'export GOROOT=/usr/lib/golang',
-  }
-  file { "${userhome}/projects" :
-    ensure => directory,
-    owner  => $user,
-    group  => $user,
-    mode   => '0755',
+    #line => 'export GOROOT=/usr/lib/golang',
+    line => 'export GOROOT=/opt/go',
   }
   file_line { "${userhome} gopath":
     path => "${userhome}/.bashrc",
-    line => "export GOPATH=${userhome}/projects",
+    line => "export GOPATH=${userhome}/go",
+  }
+  file_line { "${userhome} gobin":
+    path => "${userhome}/.bashrc",
+    line => "export GOBIN=${userhome}/go/bin",
   }
   file_line { "${userhome} userpath":
     path => "${userhome}/.bashrc",
     line => 'export PATH=$GOPATH/bin:%GOROOT/bin:$PATH',
+  }
+  $godirs = [
+              "${userhome}/go}", 
+              "${userhome}/go/bin", 
+              "${userhome}/go/pkg", 
+              "${userhome}/go/src", 
+              "${userhome}/go/projects", 
+            ]
+  file { $godirs :
+    ensure => directory,
+    owner  => $user,
+    group  => $user,
+    mode   => '0755',
   }
 }
--- modules/local/golang/manifests/install.pp
+++ modules/local/golang/manifests/install.pp
@@ -4,7 +4,8 @@ class golang::install {
   ensure_packages(['golang'], {'ensure' => 'present'})
 
   if $facts['virtual'] == 'virtualbox' {
-    $users = ['root', 'vagrant']
+    #$users = ['root', 'vagrant']
+    $users = ['vagrant']
   }
   $users.each |String $users| {
     golang::env{ $users : }
