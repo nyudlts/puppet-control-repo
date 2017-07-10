abd1aab7f4bf5846b7bc8acb4e4b63e2e10f4725
Flannon Jackson <flannon@nyu.edu>
2017-07-10 12:24:48 -0400

    Fixed typo

--- modules/site/roles/manifests/archivesspace_devenv.pp
+++ modules/site/roles/manifests/archivesspace_devenv.pp
@@ -2,6 +2,6 @@
 class roles::archivesspace_devenv {
     include profiles::base
     #require profiles::db_mysql
-    include profiles::dev_aspace_dev
+    include profiles::dev_aspace_devenv
     include profiles::rbenv_profile
 }
