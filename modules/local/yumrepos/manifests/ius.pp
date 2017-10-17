f310d489a0d24238572b8f449027be926c838a07
Flannon Jackson <flannon@nyu.edu>
2017-10-17 14:55:12 -0400

    Initial commit yumrepos

---
+++ modules/local/yumrepos/manifests/ius.pp
@@ -0,0 +1,24 @@
+#
+class yumrepos::ius {
+
+  file { '/etc/pki/rpm-gpg/IUS-COMMUNITY-GPG-KEY' :
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'https://github.com/iuscommunity-pkg/ius-release/blob/master/SOURCES/IUS-COMMUNITY-GPG-KEY',
+  }
+
+    yumrepo { 'ius.repo' :
+      name            => 'IUS Community Packages for Enterprise Linux 7 - $basearch'
+      ensure          => present,
+      descr           => 'ius',
+      baseurl         => 'https://dl.iuscommunity.org/pub/ius/stable/CentOS/7/$basearch',
+      mirrorlist      => 'https://mirrors.iuscommunity.org/mirrorlist?repo=ius-centos7&arch=$basearch&protocol=http',
+      enabled         => 1,
+      failovermethod  => 'priority',
+      protect         => 0,
+      gpgcheck        => 1,
+      metadata_expire => '30',
+    }
+}
