a61c55507fa1d1f6dd9c73335287cec1d69bab8e
Flannon Jackson <flannon@nyu.edu>
2018-07-16 21:17:37 -0400

    Added docker roles and profiles

---
+++ modules/site/profiles/manifests/build_dockerfactory.pp
@@ -0,0 +1,22 @@
+class profiles::build_dockerfactory {
+
+  #include java
+  class { 'java':
+    package => 'java-1.8.0-openjdk-devel',
+  }
+  #include pyenv
+  #pyenv::install { ['vagrant','root']: }
+  #pyenv::compile { 'compile 2.7.11 root' :
+  #  user   => 'root',
+  #  python => '2.7.11',
+  #  global => true,
+  #}
+  #pyenv::compile { 'compile 2.7.11 vagrant' :
+  #  user    => 'vagrant',
+  #  python  => '2.7.11',
+  #  global  => true,
+  #}
+
+  include docker
+}
+
---
+++ modules/site/roles/manifests/docker.pp
@@ -0,0 +1,4 @@
+class roles::docker {
+  include profiles::base
+  include profiles::build_dockerfactory
+}
